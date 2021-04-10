934d9a2d34053e39a3477a067a48433b34151b32
Federico Raso <fede.raso@hotmail.com>
2021-04-10 19:06:46 +0200

    #002

---
+++ Assets/Script/GambePlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GambePlayerMovement : MonoBehaviour
+{
+     [Header( "Movement" )]
+     [SerializeField] private float speed = 2f;
+
+     private CharacterController _controller = null;
+
+     void Start()
+     {
+          _controller = GetComponent<CharacterController>();     
+     }
+
+     void Update()
+     {
+          float xAxis = Input.GetAxis( "Horizontal" );
+          float zAxis = Input.GetAxis( "Vertical" );
+
+          Vector3 movement = new Vector3( xAxis, 0, zAxis ).normalized * speed * Time.deltaTime;
+
+          _controller.Move( movement );
+     }
+}
