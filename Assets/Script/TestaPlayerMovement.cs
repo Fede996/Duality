bd4d176f8403c96754b7108462744a606df745b9
Federico Raso <fede.raso@hotmail.com>
2021-04-11 11:57:30 +0200

    Movimento Gambe/Testa

--- Assets/Script/TestaPlayerMovement.cs
+++ Assets/Script/TestaPlayerMovement.cs
@@ -4,8 +4,30 @@ using UnityEngine;
 
 public class TestaPlayerMovement : MonoBehaviour
 {
+     [Header( "References" )]
+     [SerializeField] private Transform _camera;
+     [SerializeField] private Transform _body;
+
+     [Header( "Movement" )]
+     [SerializeField] private float sensitivityX = 2f;
+     [SerializeField] private float sensitivityY = 2f;
+
+     private float _xRot = 0;
+
+     void Start()
+     {
+          Cursor.lockState = CursorLockMode.Locked;
+          Cursor.visible = false;
+     }
+
      void Update()
      {
+          float deltaX = Input.GetAxis( "Mouse X" ) * sensitivityX * Time.deltaTime;
+          float deltaY = Input.GetAxis( "Mouse Y" ) * sensitivityY * Time.deltaTime;
+          _xRot -= deltaY;
+          _xRot = Mathf.Clamp( _xRot, -90, 90 );
 
+          _camera.localRotation = Quaternion.Euler( _xRot, 90f, 0f );
+          _body.Rotate( Vector3.up * deltaX );
      }
 }
