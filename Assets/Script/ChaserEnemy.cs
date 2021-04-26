2e4f3df2c194a7fed988aa684464d93731ca90d9
Federico Raso <fede.raso@hotmail.com>
2021-04-26 23:40:43 +0200

    Fuoco singolo/automatico

--- Assets/Script/ChaserEnemy.cs
+++ Assets/Script/ChaserEnemy.cs
@@ -3,13 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 
-[RequireComponent( typeof( Collider ) )]
 public class ChaserEnemy : Enemy
 {
-     [Server]
-     private void OnTriggerEnter( Collider other )
-     {
-          SharedCharacter player = other.GetComponent<SharedCharacter>();
-
-     }
 }
--- Assets/Script/Target.cs
+++ Assets/Script/Target.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 
+[RequireComponent( typeof( Collider ) )]
 public class Target : NetworkBehaviour
 {
      /// <summary>
